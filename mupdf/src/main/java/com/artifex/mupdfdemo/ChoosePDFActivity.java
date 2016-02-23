7c0b0defb8c971a978ed86f00a699bd13cf74dda
WuNan <me@devwu.com>
2016-02-23 21:28:27 +0800

    1.引入x86,x86_64架构的libmupdf.so文件 2.删除多余的res/string中多余的app_name字段

--- mupdf/src/main/java/com/artifex/mupdfdemo/ChoosePDFActivity.java
+++ mupdf/src/main/java/com/artifex/mupdfdemo/ChoosePDFActivity.java
@@ -91,10 +91,6 @@ public class ChoosePDFActivity extends ListActivity {
 		mUpdateFiles = new Runnable() {
 			public void run() {
 				Resources res = getResources();
-				String appName = res.getString(R.string.app_name);
-				String version = res.getString(R.string.version);
-				String title = res.getString(R.string.picker_title_App_Ver_Dir);
-				setTitle(String.format(title, appName, version, mDirectory));
 
 				mParent = mDirectory.getParentFile();
 
